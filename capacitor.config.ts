9cf41a437ed00c7d8593ae36fce0fe322eb0f1d0
jeremiasleite <jeremias23456@gmail.com>
2022-05-16 17:19:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gescolar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
